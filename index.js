4826108638dd71ec9c2461be1d4607872460222e
Official-Dev-Vineet <viratsinghkaharwar8923@gmail.com>
2024-05-13 18:52:51 +0530

    file server access

---
+++ index.js
@@ -0,0 +1,46 @@
+const { Client } = require('ssh2');
+const { config } = require('dotenv')
+
+
+const conn = new Client();
+config();
+conn.on('ready', () => {
+  console.log('Client :: ready');
+
+  // Execute a command after the connection is ready
+  conn.exec('uptime', (err, stream) => {
+    if (err) throw err;
+    stream.on('close', (code, signal) => {
+      console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
+      stream.end();
+    }).on('data', (data) => {
+      console.log('STDOUT: ' + data);
+    }).stderr.on('data', (data) => {
+      console.error('STDERR: ' + data);
+    });
+  });
+
+  // Request SFTP after the connection is ready
+  conn.sftp((err, sftp) => {
+    if (err) throw err;
+    sftp.readdir('/root', (err, files) => {
+      if (err) throw err;
+      // download files from remote server
+      console.log(files);
+      files.forEach(file => {
+        // sftp.fastGet('/root/' + file, 'download/' + file);
+        console.log(file);
+      })
+      conn.end(); // End the connection after handling files
+    });
+  });
+
+}).on('error', (err) => {
+  console.log('Client :: error :: ' + err);
+}).connect({
+  host: process.env.SSH,
+  port: process.env.SSHPORT,
+  username: process.env.SSHUSERNAME,
+  password: process.env.SSHPASSWORD  // Ensure you provide a password if required
+});
+
